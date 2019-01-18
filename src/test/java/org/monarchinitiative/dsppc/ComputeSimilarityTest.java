9e112323acaaf2d61bd2eab8fdc849a7c4cf5443
Hannah Blau <hannah.blau@jax.org>
2019-01-18 15:30:45 -0500

    Restructured the directories so automated creation of test classes would work correctly.

---
+++ src/test/java/org/monarchinitiative/dsppc/ComputeSimilarityTest.java
@@ -0,0 +1,26 @@
+package org.monarchinitiative.dsppc;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/*
+ * created 14 Jan 2019
+ */
+public class ComputeSimilarityTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void run() {
+        assertTrue(1==1);
+    }
+}
